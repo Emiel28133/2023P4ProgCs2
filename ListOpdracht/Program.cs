f0b689b811c918885ed99eb2597412b9ef4e6adc
Emiel28133 <28133@ma-web.nl>
2024-05-16 14:46:25 +0200

    06 lists practice

---
+++ ListOpdracht/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ListOpdracht
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> birds = new List<string> { "uil", "kraai", "hond", "papegaai" };
+
+            List<string> birds2 = new List<string> { "meeuw", "duif" };
+
+            birds.AddRange(birds2);
+
+            birds.Remove("hond");
+
+            birds.ForEach(Console.WriteLine);
+        }
+    }
+
+}
+
+
